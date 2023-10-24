36474b89d20349eddb8fbc86dad392c2ebce5b57
KappaXDash <m.h.oliinyk@student.khai.edu>
2023-10-24 20:58:17 +0200

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,12 @@
+package ChatBot;
+
+public class ChatBot {
+
+    public static void main(String[] args) {
+
+        String botname = "JabbaBot";
+
+        System.out.println("Hello! My name is " + botname + ".");
+        System.out.println("I was created in " + 2023 + ".");
+    }
+}
