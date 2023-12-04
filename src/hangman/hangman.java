84695fd688f6999d38a6e66b39e1a40459f4ac70
KappaXDash <m.h.oliinyk@student.khai.edu>
2023-12-04 19:30:08 +0100

    2nd-step

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -1,8 +1,24 @@
 package hangman;
+import java.util.Scanner;
+
 
 public class hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon.");
+
+        String secretWord = "bimba";
+
+        System.out.print("Guess the word:> ");
+
+        Scanner scanner = new Scanner(System.in);
+        String userGuess = scanner.nextLine();
+
+        if (userGuess.equalsIgnoreCase(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+        scanner.close();
     }
 }
