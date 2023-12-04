34df99f0dcf9cf9c1e5384746ea37a35e5768385
KappaXDash <m.h.oliinyk@student.khai.edu>
2023-12-04 19:43:18 +0100

    3rd-step

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -1,13 +1,17 @@
 package hangman;
 import java.util.Scanner;
-
+import java.util.Random;
 
 public class hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon.");
 
-        String secretWord = "bimba";
+        String[] words = {"python", "java", "javascript", "kotlin"};
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length);
+        String secretWord = words[randomIndex];
 
         System.out.print("Guess the word:> ");
 
